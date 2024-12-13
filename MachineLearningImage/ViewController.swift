4bc592a29bf0aac18f1d30092fba0b3f4b398935
xecxecikk <xecekrdnz_94@outlook.com>
2024-12-13 18:33:24 +0300

    Initial Commit

---
+++ MachineLearningImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningImage
+//
+//  Created by XECE on 13.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
